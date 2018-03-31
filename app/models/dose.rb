95f17254d748e9574ce763db66d661f7f5299786
Marcos Scorzoni <mlessa@gmail.com>
2018-03-31 15:59:43 -0300

    Validation Complete

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ApplicationRecord
-  belongs_to :cocktails
-  belongs_to :ingredients
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
