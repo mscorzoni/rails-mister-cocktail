e3a7f053d2def2e30bed28d065d769b1deb0be31
Marcos Scorzoni <mlessa@gmail.com>
2018-03-31 16:57:19 -0300

    Seed Implementation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+cocktails = [
+  {
+    name: "Old Fashioned",
+    },
+  {
+    name: "Daiquiri",
+    },
+  {
+    name: "Margarita",
+    }
+  
+]
+
+ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum)
+
